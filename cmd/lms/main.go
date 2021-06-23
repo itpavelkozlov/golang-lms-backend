627b1f7e75b1e071dffe18d56c62f87ab9307e52
Pavel Kozlov <progerdays@ya.ru>
2021-06-23 21:47:29 +0300

    added zap logger

--- cmd/lms/main.go
+++ cmd/lms/main.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/itpavelkozlov/golang-lms-backend/pkg/config"
+	"github.com/itpavelkozlov/golang-lms-backend/pkg/logger"
+	"go.uber.org/zap"
 	"log"
-	"os"
 )
 
 func main() {
 	var path = flag.String("config", "", "Path to config file")
 	flag.Parse()
 	if *path == "" {
-		os.Exit(1)
+		log.Fatalf("Please, provide --config")
 	}
 
 	c, err := config.NewConfig(*path)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	fmt.Println(c.Service.Name)
+
+	l, err := logger.NewLogger(c)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	l.Info("Hello World", zap.String("Name", c.Service.Name))
+
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,7 +9,17 @@ type Config struct {
 }
 
 type Service struct {
-	Name string `yaml:"name"`
+	Name   string `yaml:"name"`
+	Logger Logger
+}
+
+type Logger struct {
+	Cores      []string
+	Level      string
+	Filename   string
+	MaxSize    int
+	MaxBackups int
+	Compress   bool
 }
 
 func NewConfig(path string) (*Config, error) {
